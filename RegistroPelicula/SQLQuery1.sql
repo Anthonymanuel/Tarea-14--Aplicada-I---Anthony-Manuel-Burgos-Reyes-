b2241ab4dbb794e5168614e19936f94354cb26dd
Anthonymanuel <anthonyburgosreyes@gmail.com>
2015-10-18 11:07:30 -0400

    Modificando SQLQuery

--- RegistroPelicula/SQLQuery1.sql
+++ RegistroPelicula/SQLQuery1.sql
@@ -2,7 +2,7 @@ create database Pelicula
 use Pelicula
 
 create table Pelicula(
-PeliculaId int identity,
+PeliculaId int identity(1,1) primary key,
 Titulo varchar(40) not null,
 Descripcion varchar(100),
 Ano int not null,
@@ -12,4 +12,18 @@ CategoriaId int,
 Foto varchar(200),
 Video varchar(200)
 
+)
+Create table Generos(
+GeneroId int identity(1,1) primary key,
+Descripcion  varchar(60)
+)
+
+create table Estudios(
+EstudioId int identity(1,1),
+NombreEstudio varchar(100)
+)
+
+create table Actores(
+ActorId int identity(1,1) primary key,
+Nombre varchar(150)
 )
